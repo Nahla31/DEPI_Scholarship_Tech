c5e514d9aba14c4bb0354de8c025a0c3b5c0557a
Nahla Gamal <131793122+Nahla31@users.noreply.github.com>
2024-07-19 13:25:57 +0300

    Add files via upload

---
+++ Quiz/FrontEndExam-1/quiz.js
@@ -0,0 +1,53 @@
+////Question true or false
+//1 false
+//2 true
+//3 true
+//4 false
+//5 false
+//7 true
+//8 true
+
+////Question What is the output?
+/**
+ * 2)
+ * "Lydia Hallie"
+ *
+ *
+ * 3)
+ * 6
+ * 8
+ *
+ *
+ * 4)
+ * error because colorChange is a static method
+ *
+ *
+ * 5)
+ * boolean
+ * string
+ *
+ *
+ * 6)
+ * 0
+ * 1
+ * 2
+ * 3
+ * 4
+ *
+ *
+ * 7)
+ * i is not defined
+ *
+ *
+ * 8)
+ * undefined
+ * 10
+ *
+ *
+ * 9)
+ * [59.52, 83.7, 93]
+ *
+ *
+ * 10)
+ * ["bat", "bane"]
+ */
